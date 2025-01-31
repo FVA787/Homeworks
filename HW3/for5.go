7cc1a398f36d8605506dddadc13dac452ca1279d
FVA787 <fva787@gmail.com>
2025-02-01 00:15:46 +0300

    Second step at HW3

--- HW3/for5.go
+++ HW3/for5.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 8; i++ {
-		if i%2 != 0 {
-			fmt.Printf("#")
-		} else {
-			fmt.Printf(" ")
+	for i := 1; i < 8; i++ {
+		for i := 1; i <= 8; i++ {
+			if i%2 != 0 {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf(" ")
+			}
 		}
 	}
+
 }
