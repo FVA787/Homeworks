781db688dfa2498cb91481c4e04ea5678647e0c0
FVA787 <fva787@gmail.com>
2025-02-01 01:09:49 +0300

    4th commit at HW3

--- HW3/for5.go
+++ HW3/for5.go
@@ -6,13 +6,13 @@ func main() {
 	for i := 1; i <= 8; i++ {
 		for i := 1; i <= 8; i++ {
 			if i%2 != 0 {
-				if i%8 == 0 {
+				if i == 8 {
 					fmt.Printf("#\n")
 				} else {
 					fmt.Printf("#")
 				}
 			} else {
-				if i%8 == 0 {
+				if i == 8 {
 					fmt.Printf(" \n")
 				} else {
 					fmt.Printf(" ")
