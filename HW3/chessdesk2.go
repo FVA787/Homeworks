b09f0d0d42dde00eb2a8de0c1519384a9b59c453
FVA787 <fva787@gmail.com>
2025-02-03 12:27:35 +0300

    updated the code and added an input of lines and tables number from console

--- HW3/chessdesk2.go
+++ HW3/chessdesk2.go
@@ -4,16 +4,17 @@ import "fmt"
 
 func main() {
 
-	//x, y := "#", " "
-
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
+	var lines, tables int
+	fmt.Print("Enter the number of lines and tables (Format: lines, tables): ")
+	fmt.Scanf("%d, %d", &lines, &tables)
+	for i := 0; i < lines; i++ {
+		for j := 0; j < tables; j++ {
 			if (i+j)%2 == 0 {
 				fmt.Print("#")
 			} else {
 				fmt.Print(" ")
 			}
 		}
-		fmt.Println()
+		fmt.Println() //Функция Println в языке программирования Go автоматически добавляет символ новой строки в конце вывода.
 	}
 }
