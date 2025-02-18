6488499756f40dfba36127908d803b79b1928cd5
FVA787 <fva787@gmail.com>
2025-02-18 18:30:35 +0300

    how do pointers work in GoLang

--- Training/structure/pointers.go
+++ Training/structure/pointers.go
@@ -7,7 +7,11 @@ func main() {
 	xPtr := &x // тип *int
 	var p *int // тип *int, значение nil
 
+	ddx := new(int)
+	*ddx = 42
+
 	fmt.Println(xPtr)  // 0xc000090020
 	fmt.Println(*xPtr) // 2524
 	fmt.Println(p)     // nil
+	fmt.Println(*ddx)
 }
