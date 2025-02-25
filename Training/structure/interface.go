9326b92607319fb09db8167afee4c011859b3aa4
FVA787 <fva787@gmail.com>
2025-02-25 11:54:54 +0300

    fixed bugs

--- Training/structure/interface.go
+++ Training/structure/interface.go
@@ -28,7 +28,27 @@ func main() {
 	b = "Hello world"
 	fmt.Println(b)
 	fmt.Println()
-	//var a any
-	//a = 123
+
+	var v any
+	v = 1003087
+	q, ok := v.(int) // здесь мы достали из интерфейса (то же самое, что тип any) интовое значение, то есть привели значение в интерфейсе к типу int.
+	if ok {
+		fmt.Println("q is an integer:", q)
+	}
+	fmt.Println(v)
+	fmt.Println(q)
+
+	switch a.(type) {
+	case int:
+		fmt.Println("int is an integer, value is:", a)
+	case string:
+		fmt.Println("string is a string, value is:", a)
+	case float64:
+		fmt.Println("float64 is a float, value is:", a)
+	case struc.Vova:
+		fmt.Println("Type is Vova, value is", a)
+	default:
+		fmt.Println("Type is unknown")
+	}
 
 }
