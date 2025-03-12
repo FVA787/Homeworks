f76ca5ffee5e679ee0475c90e5d28e1ba7048947
FVA787 <fva787@gmail.com>
2025-03-12 15:36:40 +0300

    addition

--- Training/Functions/interface.go
+++ Training/Functions/interface.go
@@ -5,27 +5,35 @@ import (
 	"math"
 )
 
-type Shape interface {
+type shape interface { //объявляем интерфейс, который будет иметь только 1 метод area, который возвращает площадь фигуры с типом float32
 	area() float32
 }
 
-type Square struct {
+type square struct { // объявляем тип square - структуру, которая имеет тоолько одно поле sideLength с типом float32
 	sideLength float32
 }
 
-func (s Square) Area() float32 {
-	return s.sideLength * s.sideLength
+func (s square) area() float32 { // функция с аргументом в виде переменной s (мы ее не объявляли, но здесь же, видимо, мы ее и объявляем),
+	// которая (переменная) имеет тип square
+	return s.sideLength * s.sideLength // поскольку у типа square есть единственный параметр sideLength мы пишем s.sideLength
 }
 
-type Circle struct {
+type circle struct { // эти две структуры по умолчанию реализуют интерфейс shape, поскольку мы объявиил для них метод area с возвращаемым значением типа float32
 	radius float32
 }
 
-func (c Circle) Area() float32 {
+func (c circle) area() float32 {
 	return c.radius * c.radius * float32(math.Pi)
 }
 
 func main() {
-	fmt.Println(Square{4})
-	fmt.Println(Circle{4})
+	square := square{sideLength: 5}
+	circle := circle{radius: 5}
+
+	fmt.Println(square.area())
+	fmt.Println(circle.area())
+}
+
+func printShapeArea(s shape) {
+
 }
