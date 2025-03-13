c10159b2044512b0bf081a2a4185b3dfaafdd99b
FVA787 <fva787@gmail.com>
2025-03-13 13:38:04 +0300

    Added Novorossiysk project example

--- Training/Functions/methods.go
+++ Training/Functions/methods.go
@@ -11,8 +11,8 @@ func (d Lot) Profit() float64 {
 	return d.Sale_price*0.93 - d.Buy_price
 }
 func main() {
-	Novorossiysk := Lot{3825156, 7500000} // Вводим переменную лота - какие именно у него цена покупки и продажи
-	fmt.Println(Novorossiysk.Profit())    // Печать: Внутри синтаксиса:  Объект.Метод: Объект - Здание (Novorossiysk), метод - Profit,
+	Novorossiysk := Lot{3825156, 7500000}                                // Вводим переменную лота - какие именно у него цена покупки и продажи
+	fmt.Printf("Прибыль по проекту: %.1f рублей", Novorossiysk.Profit()) // Печать: Внутри синтаксиса:  Объект.Метод: Объект - Здание (Novorossiysk), метод - Profit,
 	// который описан выше в функции func (d Lot) Profit() int  (return d.Sale_price - d.Buy_price). Метод - это функция внутри
 	// структуры, которая может быть вызвана только структурой, и ниоткуда больше.
 }
